94523bb447b787490da47e1182b5a088e5edd338
daCruzNobre <98597465+daCruzNobre@users.noreply.github.com>
2024-06-30 19:12:29 -0400

    Implementat url extractor in node

---
+++ app/app.js
@@ -0,0 +1,22 @@
+import  fs from 'node:fs/promises';
+
+let filepath = "URLParser/TestFile.txt";
+const regex = /https?:\/\/(?:cloud-3\.steamusercontent\.com\/ugc\/\d+\/[A-Z0-9]+|api\.tabletopsimulator\.com(?:\/ui\/introUI)?|i\.imgur\.com\/[A-Za-z0-9]+\.jpg)\/?/g;
+
+// function that reads the contents of the file
+async function readFile(filepath) {
+    try {
+        const data = await fs.readFile(filepath, 'utf-8');
+        return data;
+    } catch (error) {
+        console.error("Error reading file:", error);
+        return null;
+    }
+
+};
+
+const fileContents = await readFile(filepath);
+let url = fileContents.match(regex);
+console.log(url);
+// console.log(fileContents.search(regex));
+

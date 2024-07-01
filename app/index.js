5c03438c9d15e9ebb0dd976836d4d5011244248a
daCruzNobre <98597465+daCruzNobre@users.noreply.github.com>
2024-07-01 16:10:23 -0400

    update regex

--- app/index.js
+++ app/index.js
@@ -2,22 +2,19 @@ const form = document.querySelector(".form");
 const inputFile= document.querySelector("input[type='file']");
 const submitBtn = document.querySelector(".submit");
 const mainContainer = document.querySelector(".main");
-const regex = /https?:\/\/(?:cloud-3\.steamusercontent\.com\/ugc\/\d+\/[A-Z0-9]+|api\.tabletopsimulator\.com(?:\/ui\/introUI)?|i\.imgur\.com\/[A-Za-z0-9]+\.jpg)\/?/g;
+// const regex = /https?:\/\/(?:cloud-3\.steamusercontent\.com\/ugc\/\d+\/[A-Z0-9]+|api\.tabletopsimulator\.com(?:\/ui\/introUI)?|i\.imgur\.com\/[A-Za-z0-9]+\.jpg)\/?/g;
+const regex = /https?:\/\/(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+/g;
 
 submitBtn.addEventListener("click", (event) =>{
     event.preventDefault();
-    if(form.fileSelector.value !== undefined){  
-        const newPara = document.createElement("p");
-        newPara.textContent = "Funfou";
-        mainContainer.appendChild(newPara);
-        console.log(typeof(url));
-        
+    // check if file was chosen
+    // read file, find urls in file, return
+    if(form.fileSelector.value !== undefined){     
         const reader = new FileReader();
         reader.onload = function () {
-            let url = reader.result.match(regex);
-            url = url.toString();
+            let url = reader.result.match(regex);        
             console.log(url);
-            console.log(typeof(url));
+            
         }
         const fileContents = reader.readAsText(inputFile.files[0]);
         // console.log(inputFile.files[0]);
--- app/parser.service.js
+++ app/parser.service.js
@@ -1,20 +1,13 @@
-
 const regex = /https?:\/\/(?:cloud-3\.steamusercontent\.com\/ugc\/\d+\/[A-Z0-9]+|api\.tabletopsimulator\.com(?:\/ui\/introUI)?|i\.imgur\.com\/[A-Za-z0-9]+\.jpg)\/?/g;
 
 // function that reads the contents of the file
-async function parseUrl(filepath) {
-    try {
-        const data = await fs.readFile(filepath, 'utf-8');
-        return data;
-    } catch (error) {
-        console.error("Error reading file:", error);
-        return null;
-    }
+ export async function parseUrl(filepath) {
+   
+     const fileContents = await readFile(filepath);
+     let url = fileContents.match(regex);
+     console.log(url);
+     // console.log(fileContents.search(regex));
 
 };
 
-const fileContents = await readFile(filepath);
-let url = fileContents.match(regex);
-console.log(url);
-// console.log(fileContents.search(regex));
 

237dbfe009baa3ac11688312846668dfe05f1476
daCruzNobre <98597465+daCruzNobre@users.noreply.github.com>
2024-07-02 10:42:10 -0400

    finish url extractor and basic style

--- app/index.js
+++ app/index.js
@@ -2,7 +2,7 @@ const form = document.querySelector(".form");
 const inputFile= document.querySelector("input[type='file']");
 const submitBtn = document.querySelector(".submit");
 const mainContainer = document.querySelector(".main");
-// const regex = /https?:\/\/(?:cloud-3\.steamusercontent\.com\/ugc\/\d+\/[A-Z0-9]+|api\.tabletopsimulator\.com(?:\/ui\/introUI)?|i\.imgur\.com\/[A-Za-z0-9]+\.jpg)\/?/g;
+const copyBtn = document.querySelector(".copy")
 const regex = /https?:\/\/(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+/g;
 
 submitBtn.addEventListener("click", (event) =>{
@@ -12,13 +12,17 @@ submitBtn.addEventListener("click", (event) =>{
     if(form.fileSelector.value !== undefined){     
         const reader = new FileReader();
         reader.onload = function () {
-            let url = reader.result.match(regex);        
-            console.log(url);
-            
+            let url = reader.result.match(regex);
+            const urlDiv = document.createElement("div");
+            urlDiv.classList.add("scrollable")
+            url.forEach(url => {
+                urlDiv.textContent += (url + "\n"); 
+            });
+            mainContainer.appendChild(urlDiv);
+                       
         }
         const fileContents = reader.readAsText(inputFile.files[0]);
-        // console.log(inputFile.files[0]);
-        // console.log(fileContents);
-        // console.log(typeof(fileContents));
+        
     };
 });
+
